3bd47bf29b9de611386c3b05cd7f8bca18ecf64b
mcalvarez <miguel.callon.alvarez@gmail.com>
2015-04-28 16:18:32 +0200

    Añadido nuevo metodo para comprobar la distancia

--- src/main/java/es/uvigo/esei/daa/dao/EventDAO.java
+++ src/main/java/es/uvigo/esei/daa/dao/EventDAO.java
@@ -1,15 +1,21 @@
 package es.uvigo.esei.daa.dao;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import es.uvigo.esei.daa.bean.EventFilterBean;
+import es.uvigo.esei.daa.bean.PagBean;
 import es.uvigo.esei.daa.entities.Event;
+import es.uvigo.esei.daa.entities.Location;
+import es.uvigo.esei.daa.util.LocationUtil;
 
 @Repository
 public class EventDAO extends DAO {
@@ -35,10 +41,32 @@ public class EventDAO extends DAO {
 	}
 	
 	public List<Event> getAllEvents(
-			SearchFiltersBean $busquedaSubastasBean, 
+			EventFilterBean eventFilterBean, 
 			PagBean pagBean) {
+		List<Event> eventList = null;
+		Session session = sessionFactory.getCurrentSession();
 		
-		// Filters
+		int firstElement = pagBean.getNumElemPag() 
+				* ( (pagBean.getNumPag() - 1) +1 );
 		
+		eventList = session.createCriteria(Event.class).
+			setFirstResult(firstElement).
+			setMaxResults(pagBean.getNumElemPag()).
+			addOrder(Order.desc("date"))
+			.list();
+		
+		for (Event event:eventList) {
+			Location dstlocation = event.getLocation();
+			if (LocationUtil.checkDistance(eventFilterBean.getSrcLocation(),
+					dstlocation, eventFilterBean.getMaxDistance())) {
+				event.setDistanceFromSrc(
+						LocationUtil.getDistance(
+								eventFilterBean.getSrcLocation(),
+								dstlocation));
+			}
+		}	
+		Collections.sort(eventList);
+		
+		return eventList;
 	}
 }
